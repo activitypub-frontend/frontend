dc4834f1f6f06da4b94db697bd1cf2c1fdd71bb1
Marvin Gaube <dev@marvingaube.de>
2019-05-29 22:35:29 +0200

    Some more click foo

--- scripts/mastodon.mjs
+++ scripts/mastodon.mjs
@@ -11,10 +11,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
     },
     methods: {
       mastodonLogin: function(event) {
-                console.log("Auth");
+        console.log("Auth");
         const mInstance = document.getElementById('mastodonInstance').value;
         console.log("Auth");
         doMastodonAuth(mInstance);
+      },
+      say: function(message) {
+        alert(message)
       }
     }
   });
