d2f8324ba66fbb22dcbcbae9ee71cdb256b3eb97
Marvin Gaube <dev@marvingaube.de>
2019-05-30 13:10:48 +0200

    Try to fix author position

--- scripts/mastodon.mjs
+++ scripts/mastodon.mjs
@@ -97,8 +97,8 @@ function mRenderStatus(s) {
   let htmlStatus = "<div class='mStatus' id='" + s.id + "'>";
   // From
   htmlStatus += "<span class='mAuthorImgBox'><a href='"+s.account.url+"'' target='_blank' class='mAuthorImgLink'><img src='" + s.account.avatar + "' class='mAuthorImg'></a></span>";
-  htmlStatus += "<span class='mAuthor'><a href='"+s.account.url+"'' target='_blank' class='mAuthorLink'>" + s.account.display_name + "</a></span>";
-  htmlStatus += "<span class='mAuthorUser'><a href='"+s.account.url+"'' target='_blank' class='mAuthorLink'>@" + s.account.username + "</a></span>";
+  htmlStatus += "<span class='mAuthor'><a href='"+s.account.url+"'' target='_blank' class='mAuthorLink'>" + s.account.display_name + "</a><br>";
+  htmlStatus += "< class='mAuthorUser mAuthorLink' href='"+s.account.url+"'' target='_blank'>@" + s.account.username + "</a></span>";
   htmlStatus += "<span class='mCreated'>" + formatDate(new Date(s.created_at)) + "</span>";
   htmlStatus += "<p class='mContent'>" + s.content + "</p>";
   if(s.media_attachments[0] && s.media_attachments[0].type === "image") {
